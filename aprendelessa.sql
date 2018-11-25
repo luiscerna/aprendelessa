bd2ad0fbd0b603ac8e6e0ef859361b29972d9b0b
hijpax <44858275+hijpax@users.noreply.github.com>
2018-11-25 17:42:13 -0600

    editando aprendelessa.sql

--- aprendelessa.sql
+++ aprendelessa.sql
@@ -1,26 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `aprendelessa` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `aprendelessa`;
--- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: aprendelessa
--- ------------------------------------------------------
--- Server version	8.0.12
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `coincidencia`
---
-
 DROP TABLE IF EXISTS `coincidencia`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
